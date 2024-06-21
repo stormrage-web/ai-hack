670c2b8558988306b703e73c7198c0a230d1d11d
stormrage-web <sharfull33@gmail.com>
2024-06-21 16:27:00 +0300

    chore: added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
+import "./App.css";
 import HomePage from "./pages/HomePage/HomePage.tsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CasePage from "./pages/CasePage/CasePage.tsx";
-import {ConfigProvider} from "antd";
-import './assets/embla.css';
+import { ConfigProvider } from "antd";
+import "./assets/embla.css";
 import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 import PrivateRoute from "./ui/PrivateRoute/PrivateRoute.tsx";
 import UserProvider from "./contexts/UserContextProvider.tsx";
@@ -12,26 +12,26 @@ import CasesProvider from "./contexts/CasesContextProvider.tsx";
 
 function App() {
 
-    return (
-        <div className="app">
-            <ConfigProvider theme={{token: {colorPrimary: '#4F6AE8',}}}>
-                <CasesProvider>
-                    <UserProvider>
-                        <BrowserRouter>
-                            <Routes>
-                                <Route path="/" element={<PrivateRoute/>}>
-                                    <Route path="home" element={<HomePage/>}/>
-                                    <Route path="case/:caseId" element={<CasePage/>}/>
-                                </Route>
-                                <Route path="login" element={<LoginPage/>}/>
-                                <Route path='*' element={<NotFoundPage/>}/>
-                            </Routes>
-                        </BrowserRouter>
-                    </UserProvider>
-                </CasesProvider>
-            </ConfigProvider>
-        </div>
-    )
+	return (
+		<div className="app">
+			<ConfigProvider theme={{ token: { colorPrimary: "#4F6AE8" } }}>
+				<CasesProvider>
+					<UserProvider>
+						<BrowserRouter>
+							<Routes>
+								<Route path="/" element={<PrivateRoute />}>
+									<Route path="home" element={<HomePage />} />
+									<Route path="case/:caseId" element={<CasePage />} />
+								</Route>
+								<Route path="login" element={<LoginPage />} />
+								<Route path="*" element={<NotFoundPage />} />
+							</Routes>
+						</BrowserRouter>
+					</UserProvider>
+				</CasesProvider>
+			</ConfigProvider>
+		</div>
+	);
 }
 
-export default App
+export default App;
